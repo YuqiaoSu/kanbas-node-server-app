f6dbf19f6588b9d64a381c32b3d4a0a60ff613e6
yuqiaosu-toast <Simon.Suyq@gmail.com>
2023-11-05 20:43:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import cors from "cors";
-import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
-import AssignmentRoutes from "./assignment/routes.js";
+import CourseRoutes from "courses/routes.js";
+import ModuleRoutes from "Modules/routes.js";
+import AssignmentRoutes from "assignment/routes.js";
 import "dotenv/config";
 
 const app = express();
