361a7676a865cd2ab1fc8bcf15d862b2e460159c
yuqiaosu-toast <Simon.Suyq@gmail.com>
2023-11-07 09:44:51 -0500

    deploy fix attempt

--- Database/index.js
+++ Database/index.js
@@ -1,3 +1,4 @@
+import assert from 'assert';
 import courses from "./courses.json" assert { type: "json" };
 import modules from "./modules.json" assert { type: "json" };
 import assignments from "./assignments.json" assert { type: "json" };
