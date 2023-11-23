7754b9f5149ef55d9c4a0b88ff042a636b2cfccc
yuqiaosu-toast <Simon.Suyq@gmail.com>
2023-11-23 11:37:36 -0500

    1

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignment/routes.js";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(
@@ -15,7 +20,16 @@ app.use(
         origin: [process.env.FRONTEND_URL_DEPLOY, process.env.FRONTEND_URL_DEVELOPMENT]
     })
 );
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+app.use(
+    session(sessionOptions)
+);
 
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
